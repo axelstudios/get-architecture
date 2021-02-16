4056660994b60e293daf1bba706448f1deb052cc
Alex Swindler <Alex.Swindler@nrel.gov>
2021-02-15 18:18:52 -0700

    updated dependencies

--- index.js
+++ index.js
@@ -3,34 +3,38 @@ const fs = require('fs');
 module.exports = file => {
   return new Promise((resolve, reject) => {
     // Check that file is readable
-    fs.access(file, fs.constants.F_OK | fs.constants.R_OK, err => {
-      if (err) {
-        reject(new Error(`${file} ${err.code === 'ENOENT' ? 'does not exist' : 'is not readable'}`));
+    fs.access(file, fs.constants.F_OK | fs.constants.R_OK, error => {
+      if (error) {
+        reject(new Error(`${file} ${error.code === 'ENOENT' ? 'does not exist' : 'is not readable'}`));
         return;
       }
+
       // Open file
-      fs.open(file, 'r', (err, fd) => {
-        if (err) {
-          reject(err);
+      fs.open(file, 'r', (error, fd) => {
+        if (error) {
+          reject(error);
           return;
         }
+
         // Determine offset to PE header
         let buffer = Buffer.alloc(4);
-        fs.read(fd, buffer, 0, 4, 0x3C, err => {
-          if (err) {
-            reject(err);
+        fs.read(fd, buffer, 0, 4, 0x3C, error => {
+          if (error) {
+            reject(error);
             return;
           }
+
           // Read PE header
           const offset = hexToDec(littleEndian(buffer));
           buffer = Buffer.alloc(2);
-          fs.read(fd, buffer, 0, 2, offset + 0x18, err => {
-            if (err) {
-              reject(err);
+          fs.read(fd, buffer, 0, 2, offset + 0x18, error => {
+            if (error) {
+              reject(error);
               return;
             }
+
             const hex = littleEndian(buffer);
-            const result = hex === '020b' ? 'x64' : hex === '010b' ? 'x86' : null;
+            const result = hex === '020b' ? 'x64' : (hex === '010b' ? 'x86' : null);
 
             if (result === null) {
               reject(new Error('Unknown'));
@@ -49,5 +53,5 @@ function littleEndian(buffer) {
 }
 
 function hexToDec(hex) {
-  return parseInt(hex, 16);
+  return Number.parseInt(hex, 16);
 }
